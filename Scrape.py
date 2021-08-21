e62b1be176d6b0eb053c364aa402b516d11c41c0
Vishnu Nair <40653379+vnnair@users.noreply.github.com>
2021-08-20 22:32:24 -0500

    Update Scrape.py

--- Scrape.py
+++ Scrape.py
@@ -13,6 +13,7 @@ from tkinter import filedialog, Text
 from PIL import ImageTk, Image
 
 
+#This is the parser to scrape product information from Ebay
 def getEbayInfo(search_term, page_range):
     urls = []
     names = []
@@ -23,22 +24,28 @@ def getEbayInfo(search_term, page_range):
     numratings = []
     remove_indices = []
     processed_ratings = []
+    #For query purposes, the spaces should be replaced with a '+'
     search_term = search_term.replace(' ', '+')
+    #Instantiating the driver (using Google Chrome as the browser)
     driver = webdriver.Chrome(executable_path='/Users/vishnunair/Desktop/chromedriver')
+    #The first string is a generic string for the Ebay domain name. We insert our search term and format by page number
     url = "https://www.ebay.com/sch/i.html?&_nkw=" + search_term + '&_sacat=0&_pgn={}'
     
+    #Iteratively scraping information based on user-input number of pages
     for page in range(1, page_range+1):
         driver.get(url.format(page))
+        #Accessing the source code of the webpage
         soup = BeautifulSoup(driver.page_source, 'html.parser')
         all_items = soup.find_all('li', attrs={'class': 's-item'})
         for index, item in enumerate(all_items):
-            
+            #Getting name and price by filtering for the following attributes
             name = item.find_all('h3', attrs={'class':"s-item__title"})[0].text
             name2 = item.find('a', attrs={'class':"s-item__link"})
             names.append(name)
             price = item.find('span', attrs={'class':"s-item__price"})
             condition = item.find('span', 'SECONDARY_INFO')
             
+            #Attribute error will occur if nothing is found
             try:
                 conditions.append(condition.text)
             except AttributeError:
@@ -48,7 +55,7 @@ def getEbayInfo(search_term, page_range):
                 prices.append(price.text)
             except AttributeError:
                 prices.append('')
-
+            #Getting average rating, number of ratings, and shipping cost
             rating = item.find('div', 'x-star-rating')
             shipping_cost = item.find('span', 's-item__shipping s-item__logisticsCost')
             rating_num = item.find('span', 's-item__reviews-count')              
@@ -69,15 +76,17 @@ def getEbayInfo(search_term, page_range):
             except AttributeError:
                 shipping.append('$0.00')
             
-            
+            #Obtaining the URL of the product
             prod_url = name2.get('href')
             urls.append(prod_url)
                 
-                
+    #Removing observations that have price ranges instead of a concrete price 
+    #Removing empty observations as well
     for index, price in enumerate(prices):
         if price == '' or 'to' in price:
             remove_indices.append(index)
-            
+    
+    #Removing all observations that don't meet this criteria
     names = [i for j, i in enumerate(names) if j not in remove_indices]
     prices = [i for j, i in enumerate(prices) if j not in remove_indices]
     ratings = [i for j, i in enumerate(ratings) if j not in remove_indices]
@@ -94,7 +103,7 @@ def getEbayInfo(search_term, page_range):
         except AttributeError:
             processed_ratings.append(np.nan)
             
-    
+    #Finally, we append to a pandas dataframe
     result = pd.DataFrame({"Name":names, "Price": prices, "Rating": ratings, "Rating Count": processed_ratings,
                            "Shipping Cost": shipping, "Condition": conditions, "URL": urls})
     result = result.replace(r'^\s*$', np.nan, regex=True)
@@ -107,9 +116,8 @@ def getEbayInfo(search_term, page_range):
     return result
 
 
-
+#The Amazon webscraper
 def getAmazonInfo(search_term, page_range):
-    global name2
     names = []
     prices = []
     ratings = []
@@ -176,6 +184,7 @@ def getAmazonInfo(search_term, page_range):
     
     result = pd.DataFrame({"Name":names, "Price": prices, "Rating": ratings, "Rating Count": numratings,
                           "URL": urls})
+    #Replacing missing values with NaN's
     result = result.replace(r'^\s*$', np.nan, regex=True)
 
     driver.close()
@@ -183,6 +192,7 @@ def getAmazonInfo(search_term, page_range):
     return result
 
 
+#The main function preprocesses the dataframes obtained by the previous two functions 
 def main(search_term, pages):
     
     adrop_rows = []
@@ -190,12 +200,14 @@ def main(search_term, pages):
     ebay = getEbayInfo(search_term, pages)
     amazon = getAmazonInfo(search_term, pages)
     
+    #Filling all NaN's with 0 and formatting the 'Price' column
     amazon = amazon.fillna('0')
     ebay = ebay.fillna('0')
     
     amazon['Price'] = amazon['Price'].str.replace(',', '')
     amazon['Price'] = amazon['Price'].str.replace(',', '')
     amazon['Price'] = amazon['Price'].str.replace('$', '')
+    
     ebay['Price'] = ebay['Price'].str.replace(',', '')
     ebay['Price'] = ebay['Price'].str.replace(',', '')
     ebay['Price'] = ebay['Price'].str.replace('$', '')
@@ -208,6 +220,9 @@ def main(search_term, pages):
     
     
     amazon['Rating Count'] = amazon['Rating Count'].str.replace(',', '')
+    #Sometimes incorrect values get scraped for the ratings. We try to convert it into a float object
+    #If this is not possible, we replace the incorrect value with a 0
+    #We do this for both datasets
     for rating in amazon['Rating Count']:
         try:
             float(rating)
@@ -234,7 +249,9 @@ def main(search_term, pages):
     amazon['Rating'] = pd.to_numeric(amazon['Rating'])
     ebay['Rating'] = pd.to_numeric(ebay['Rating'])
     
-    
+    #When we query we often come across products that are not too related to our search
+    #Here, we take each word of our search term and ensure that it resides in each product name that we scraped
+    #This is not a foolproof method but it still helps filter for only relevant items in our search
     for item in amazon['Name']:
         for word in search_term.split(' '):
             if word not in item:
@@ -249,23 +266,26 @@ def main(search_term, pages):
     edrop_rows.append(drop_index)
     edrop_rows = list(set(edrop_rows))
     
-    
+    #We sort the values by rating count
     amazon = amazon.drop(adrop_rows)
     amazon = amazon.sort_values(['Rating Count'], ascending = False)
     ebay = ebay.drop(edrop_rows)
     ebay = ebay.sort_values(['Rating Count'], ascending = False)
     
+    #If there is a case where all the ratings do not exist, we sort by the ratings
     if (ebay['Rating'] == 0).all() == True:
         ebay = ebay.sort_values(['Price'], ascending = False)
     if (amazon['Rating'] == 0).all() == True:
         amazon = amazon.sort_values(['Price'], ascending = False)
     
+    #Getting the top 10 items
     ebay = ebay.head(10)
     amazon = amazon.head(10)
     
     ebay['Price'] = ebay['Price'].map('${:,.2f}'.format)
     amazon['Price'] = amazon['Price'].map('${:,.2f}'.format)
     
+    #Finally, we fill the GUI frames with the two preprocessed dataframes
     pt1 = Table(lower_frame1, dataframe=amazon)
     pt1.show()
     
@@ -280,42 +300,46 @@ def main(search_term, pages):
 
 
 
-
+#We instantiate a TkInter root, set the background to white, and instantiate an icon image
 root = tk.Tk()
 root.config(bg = "white")
 root.iconbitmap('investment_grow_money_increase_dollar_business_icon_188469.ico')
 
+#Feel free to play around with different widths and heights of your app as you please
 canvas = tk.Canvas(root, height=350, width=600)
 canvas.pack()
 
 
-
+#These two frames serve for aesthetic purposes
 f = tk.Frame(root, bg='#e3b9b3', bd=10)
 f.place(relx=.5, rely=0.0, relwidth=1.0, relheight=1.0, anchor='n')
 
 g = tk.Frame(root, bg='#97a989', bd=10)
 g.place(relx=.5, rely=0.5, relwidth=1.0, relheight=1.0, anchor='n')
 
-
+#Making the first toolbar on the page have a beige border
 frame = tk.Frame(root, bg='#eee3d6', bd=5)
 frame.place(relwidth=0.75, relheight=0.1, relx=0.5, rely=0.1, anchor= 'n')
 
-
+#Making a toolbar that allows the user to input the search term
 search_entry = tk.Entry(frame, font=40)
 search_entry.place(relwidth=0.325, relheight=1)
 
+#Allows the user to enter the max page number they wish to scrape
 page_entry = tk.Entry(frame, font=40)
 page_entry.place(relx=.4, relwidth=0.1, relheight=1)
 
+#The button function calls our main function, thereby creating the two datasets and placing them in the frames we will now create
 button = tk.Button(frame, text="Compare", padx=10, pady=5, fg='black', bg='#36384c', command = lambda: main(search_entry.get(), int(page_entry.get())))
 button.place(relx=0.7, relheight=1, relwidth=0.3)
 
+#The lower mainframe houses two sub-frames. This serves for aesthetic purpose as well
 lower_mainframe = tk.Frame(root, bg='#eee3d6', bd=10)
 lower_mainframe.place(relwidth=0.75, relheight=0.7, relx=0.5, rely=0.25, anchor='n')
 
+#Placing two frames in our GUI that will house the scraped datasets
 lower_frame1 = tk.Frame(lower_mainframe, bg='#36384c', width=150, height=200)
 lower_frame1.place(relx=0.50, rely=0.02, relwidth=1.0, relheight=0.45, anchor='n')
-
 
 lower_frame2 = tk.Frame(lower_mainframe, bg='#36384c', width=150, height=200)
 lower_frame2.place(relx=0.50, rely=0.52, relwidth=1.0, relheight=0.45, anchor='n')
